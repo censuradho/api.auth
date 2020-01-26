f7e6e75899ad085a4b86795978734ac0ea57a4a4
censuradho <gustavoleiteoliveira800@gmail.com>
2020-01-26 20:04:55 -0300

    add uma variavel de ambiente no secrete_key do jwt

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,16 +1,15 @@
 /* eslint-disable no-new */
-import Jwt from 'jsonwebtoken'
-import fs from 'fs'
+import Jwt, { Secret } from 'jsonwebtoken'
+import { ProcessEnvOptions } from 'child_process'
 
-const certPriv = fs.readFileSync(`${__dirname}/rsa-private.pem`)
-
+type Env = Secret & ProcessEnvOptions
 export const generateToken = async (payload?: any) => {
-  const token = await Jwt.sign(payload, certPriv, { algorithm: 'HS256' })
+  const token = await Jwt.sign(payload, process.env.SECRET_KEY as Env, { algorithm: 'HS256' })
   return token
 }
 
 export const verifyToken = async (token: string): Promise<string | object | boolean> => {
-  const User = await Jwt.verify(token, certPriv)
+  const User = await Jwt.verify(token, process.env.SECRET_KEY as Env)
 
   if (!User) return false
   return User
